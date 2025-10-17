62d94cb2c77e5cf0ff417956ac47f8ae13ed9a8e
User <abbymoraa876@gmail.com>
2025-10-17 20:24:00 +0300

    Installed and Imported Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen text-center">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Welcome to Journee 
+      </h1>
+    </div>
+  )
+}
+
+export default App
